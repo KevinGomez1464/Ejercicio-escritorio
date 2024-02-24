e96150e8317d6a8f38119fc0f3595d7ff9617f49
KevinGomez1464 <161092881+KevinGomez1464@users.noreply.github.com>
2024-02-23 19:30:34 -0500

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,11 @@
+num1 = 10
+num2 = 20
+suma = num1 + num2
+resta = num1 - num2
+Multiplicacion =num1 * num2
+División = num1 / num2
+
+print(suma)
+print(resta)
+print(Multiplicacion)
+print(División)
